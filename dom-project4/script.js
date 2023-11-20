4bf8473f9d14373ae1778dc2207ff75debebbd5a
SalmanAhmadphp <151534410+SalmanAhmadphp@users.noreply.github.com>
2023-11-20 18:20:34 +0530

    Add files via upload

---
+++ dom-project4/script.js
@@ -0,0 +1,31 @@
+
+var arr = [
+
+    {dp:"https://images.unsplash.com/photo-1588354924596-71b1cb1393fb?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1509479200622-4503f27f12ef?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://plus.unsplash.com/premium_photo-1682145730713-34bba6d3d14a?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1519669417670-68775a50919c?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://images.unsplash.com/photo-1501369497246-426438abca8e?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://plus.unsplash.com/premium_photo-1661764393655-1dbffee8c0ce?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://images.unsplash.com/photo-1592609931041-40265b692757?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1497215842964-222b430dc094?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+
+]
+
+var instastorys=document.querySelector("#instastorys")
+var call=""
+arr.forEach(function(elem,idx){
+
+    call += `<div class="story">
+    <img id="${idx}" src="${elem.dp}" alt="">
+</div>
+`
+})
+
+instastorys.innerHTML=call
+
+instastorys.addEventListener("click",function(dets){
+    document.querySelector("#full-screen").style.display="block"
+    document.querySelector("#full-screen").style.backgroundImage=`url(${arr[dets.target.id].story})`
+    
+    setTimeout(function(){
+        document.querySelector("#full-screen").style.display="none"
+        
+    },2000)
+});
